cdcff9b2c9005ab466e9d1a8caa74ad46fe6ce8c
cdl <1217096231@qq.com>
2020-10-08 21:13:58 +0800

    包和函数

---
+++ src/github.com/goCode/04_package_function/go.mod
@@ -0,0 +1,3 @@
+module github.com/goCode/04_package_function
+
+go 1.13
---
+++ src/github.com/goCode/04_package_function/main.go
@@ -0,0 +1,11 @@
+package main 
+
+import "fmt"  // fmt是包
+
+// 声明函数使用关键字func
+func main(){
+	fmt.Println("Hello PlayGround!")
+}
+
+// go语言中{}位置必须要注意！
+
